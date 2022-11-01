54aeaf71707b782393a2791299e0ac74c9035e76
MyoThuKha <myothukha@outlook.sg>
2022-11-01 21:38:59 +0630

    added dark mode

--- components/content.tsx
+++ components/content.tsx
@@ -2,16 +2,21 @@ import { motion } from "framer-motion";
 import React, { useState } from "react";
 import dataList from "../data/data";
 
-interface contentProps {}
+interface contentProps {
+  theme: boolean;
+  changeTheme: () => void;
+  dynamicColor: string;
+}
 
 const iconVariant = {
   hover: {
     scale: 1.1,
   },
 };
-const Content: React.FC<contentProps> = ({}) => {
+const Content: React.FC<contentProps> = ({ theme: isDark, changeTheme }) => {
   const maxIndex = dataList.length - 1;
   const [curr, setCurr] = useState(0);
+  const borderColor = isDark ? " border-white" : " border-black";
   return (
     <div className="flex items-center justify-around w-screen h-screen relative">
       <div className="absolute top-4 right-8">
@@ -22,25 +27,16 @@ const Content: React.FC<contentProps> = ({}) => {
           strokeWidth={1.5}
           stroke="currentColor"
           className="w-6 h-6"
+          onClick={() => changeTheme()}
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            d={
+              !isDark
+                ? "M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                : "M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            }
           />
         </svg>
       </div>
@@ -67,7 +63,11 @@ const Content: React.FC<contentProps> = ({}) => {
         />
       </motion.svg>
 
-      <div className="text-center h-1/3 w-1/3 border-2 border-black rounded-3xl px-8 py-4">
+      <div
+        className={
+          "text-center h-1/3 w-1/3 border-2 rounded-3xl px-8 py-4" + borderColor
+        }
+      >
         <h2 className="text-4xl py-4 font-bold">{dataList[curr].title}</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
@@ -99,20 +99,20 @@ const Content: React.FC<contentProps> = ({}) => {
       <motion.a
         variants={iconVariant}
         whileHover="hover"
-        className="btn-outline"
+        className={"btn-outline" + borderColor}
         href={dataList[curr].uri}
         target="_blank"
         rel="noreferrer"
       >
         <p>Try out</p>
-        <div className="rounded-full w-9 h-9 bg-white ml-2 flex items-center justify-center">
+        <div className="rounded-full w-9 h-9 ml-2 flex items-center justify-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-4 h-4 text-black"
+            className="w-4 h-4"
           >
             <path
               strokeLinecap="round"
--- components/hello.tsx
+++ components/hello.tsx
@@ -7,7 +7,9 @@ const Hello: React.FC<helloProps> = ({}) => {
   return (
     <div className="flex w-screen items-center justify-around">
       <h1 className="text-8xl">Projects</h1>
-      <Image src="/window.svg" alt="main image" width="400" height="400" />
+      <div className="bg-white rounded-full">
+        <Image src="/window.svg" alt="main image" width="400" height="400" />
+      </div>
     </div>
   );
 };
